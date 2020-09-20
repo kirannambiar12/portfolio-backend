eaa36ad148c394956dd629eb3381344c70451098
kirannambiar12 <kirannambiar12@gmail.com>
2020-09-20 20:25:54 +0530

    added allowed host

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://portfolio-django-backend.herokuapp.com/",
+    "https://kiran-portfolio.herokuapp.com/",
 ]
 
 REST_FRAMEWORK = {
