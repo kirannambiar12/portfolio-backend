e106742123991eff2cbebea95a3915c3c63c4050
kirannambiar12 <kirannambiar12@gmail.com>
2020-11-02 22:23:22 +0530

    changed the models for description field

--- work/models.py
+++ work/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
 
 class Service(models.Model):
     title = models.CharField(blank=False, null=False, max_length=256)
-    description = models.CharField(blank=False, null=False, max_length=256)
+    description = RichTextUploadingField()
 
     def __str__(self):
         return self.title
 
 class TechnologiesAndFramework(models.Model):
     title = models.CharField(blank=False, null=False, max_length=256)
-    description = models.CharField(blank=False, null=False, max_length=256)
+    description = RichTextUploadingField()
     service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True, related_name='technologies')
 
     def __str__(self):
--- work/serializers.py
+++ work/serializers.py
@@ -2,15 +2,14 @@ from rest_framework import serializers
 from work.models import Service, TechnologiesAndFramework
 
 
-class ServiceSerializer(serializers.ModelSerializer):
-    
-    technologies = serializers.SlugRelatedField(read_only=True, many=True, slug_field="title")
-    class Meta:
-        model = Service
-        fields = ('id', 'title', 'description', 'technologies')
-
-
 class TechnologiesAndFrameworkSerializer(serializers.ModelSerializer):
     class Meta:
         model = TechnologiesAndFramework
         fields = ('id', 'title', 'description')
+
+class ServiceSerializer(serializers.ModelSerializer):
+
+    technologies = TechnologiesAndFrameworkSerializer(read_only=True, many=True)
+    class Meta:
+        model = Service
+        fields = ('id', 'title', 'description', 'technologies')
