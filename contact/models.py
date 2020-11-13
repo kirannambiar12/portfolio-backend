d5b24fc644d092d8bd456b439919067059b090d4
kirannambiar12 <kirannambiar12@gmail.com>
2020-11-13 21:39:38 +0530

    added date field

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone, dateformat
 
 
 class Contact(models.Model):
     name = models.CharField(blank=False, null=False, max_length=256)
     email_id = models.CharField(blank=False, null=False, max_length=256)
     subject = models.TextField(blank=False, null=False)
+    date = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
 
     def __str__(self):
         return self.name
